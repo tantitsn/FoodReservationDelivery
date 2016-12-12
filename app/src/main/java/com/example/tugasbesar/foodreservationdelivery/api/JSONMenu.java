9265bfb1ab0374bedc6252ce0b91443e49dc6a65
fajarfr <fajar.bsd@gmail.com>
2016-12-12 16:23:23 +0700

    first commit hiji hiji goldar B

---
+++ app/src/main/java/com/example/tugasbesar/foodreservationdelivery/api/JSONMenu.java
@@ -0,0 +1,13 @@
+package com.example.tugasbesar.foodreservationdelivery.api;
+
+import com.example.tugasbesar.foodreservationdelivery.models.MenuItem;
+
+/**
+ * Created by uber on 12/12/16.
+ */
+public class JSONMenu {
+    private MenuItem[] posts;
+    public MenuItem[] getPosts(){
+        return posts;
+    }
+}
---
+++ app/src/main/java/com/example/tugasbesar/foodreservationdelivery/models/MenuItem.java
@@ -0,0 +1,44 @@
+package com.example.tugasbesar.foodreservationdelivery.models;
+
+/**
+ * Created by uber on 12/12/16.
+ */
+
+public class MenuItem {
+    private String id_produk;
+    private String nama_produk;
+    private String harga_produk;
+    private String gambar_produk;
+
+    public String getId_produk() {
+        return id_produk;
+    }
+
+    public void setId_produk(String id_produk) {
+        this.id_produk = id_produk;
+    }
+
+    public String getGambar_produk() {
+        return gambar_produk;
+    }
+
+    public void setGambar_produk(String gambar_produk) {
+        this.gambar_produk = gambar_produk;
+    }
+
+    public String getHarga_produk() {
+        return harga_produk;
+    }
+
+    public void setHarga_produk(String harga_produk) {
+        this.harga_produk = harga_produk;
+    }
+
+    public String getNama_produk() {
+        return nama_produk;
+    }
+
+    public void setNama_produk(String nama_produk) {
+        this.nama_produk = nama_produk;
+    }
+}
